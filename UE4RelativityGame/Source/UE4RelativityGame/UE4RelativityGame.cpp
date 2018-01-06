b8fe7e703ee4863c81c90a2160cde58282a7abef
Thomas Clark <b6015511@my.shu.ac.uk>
2018-01-06 23:43:44 +0000

    Revert "new file"
    This reverts commit f2ab373b394c5cea3c4af3496b94289dd5a905af.

--- UE4RelativityGame/Source/UE4RelativityGame/UE4RelativityGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-#include "UE4RelativityGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4RelativityGame, "UE4RelativityGame" );
- 
