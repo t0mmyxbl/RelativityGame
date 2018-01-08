11a84b31534c719d57254c868442b889470b73fa
George McDonald <b6039417@my.shu.ac.uk>
2018-01-08 16:16:06 +0000

    New unity files uploaded
    m

---
+++ RelativityUnity/Assets/Gravity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+
+	public float thrust;
+	public Rigidbody rb;
+	bool inverted;
+
+
+	void Start()
+	{
+		inverted = false;
+		rb = GetComponent<Rigidbody>();
+
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetKeyDown ("g"))
+			ChangeGravity ();
+
+		if (inverted)
+			rb.AddForce (0, -5, 0);
+		else
+			rb.AddForce (0, 5, 0);
+	}
+
+
+
+void ChangeGravity()
+{
+	if (inverted)
+		inverted = false;
+	else
+		inverted = true;
+}
+
+}
