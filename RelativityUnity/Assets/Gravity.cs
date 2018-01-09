3b69177035f17d88bc3a8b2cf81a87703f88245b
Thomas Clark <b6015511@my.shu.ac.uk>
2018-01-09 02:24:31 +0000

    trying to create player

--- RelativityUnity/Assets/Gravity.cs
+++ RelativityUnity/Assets/Gravity.cs
@@ -7,6 +7,7 @@ public class Gravity : MonoBehaviour {
 
 	public float thrust;
 	public Rigidbody rb;
+    //GameObject obj;
 	bool inverted;
 
 
@@ -14,6 +15,7 @@ public class Gravity : MonoBehaviour {
 	{
 		inverted = false;
 		rb = GetComponent<Rigidbody>();
+        //obj = GetComponent<GameObject>();
 
 	}
 
@@ -22,10 +24,23 @@ public class Gravity : MonoBehaviour {
 		if (Input.GetKeyDown ("g"))
 			ChangeGravity ();
 
-		if (inverted)
-			rb.AddForce (0, -5, 0);
-		else
-			rb.AddForce (0, 5, 0);
+        if (gameObject.tag != "Player")
+        {
+		    if (inverted)
+			    rb.AddForce (0, 5, 0);
+		    else
+			    rb.AddForce (0, -5, 0);
+        }
+        else
+        {
+            if (inverted)
+                rb.transform.Translate(0, 0.1f, 0);
+            else
+                rb.transform.Translate(0, -0.1f, 0);
+        }
+
+
+
 	}
 
 
---
+++ RelativityUnity/Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        [SerializeField] private float speed;
+
+        private Rigidbody rb;
+        //private CharacterController m_CharacterController;
+
+
+    // Use this for initialization
+    void Start()
+        {
+            //m_CharacterController = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //RotateView();
+
+            if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += Vector3.right * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position += Vector3.left * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position += Vector3.forward * speed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position += Vector3.back * speed * Time.deltaTime;
+                }
+    }
+
+    }
