4360f1be12530c238338b4b184260f619c9a5865
George McDonald <b6039417@my.shu.ac.uk>
2018-01-10 17:20:04 +0000

    Player spins but the camera "corrects" itself to be vertical according to worldspace

--- RelativityUnity/Assets/Scripts/Gravity.cs
+++ RelativityUnity/Assets/Scripts/Gravity.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityStandardAssets.Characters.FirstPerson;
 using UnityEngine;
+using UnityStandardAssets.Utility;
 
 public class Gravity : MonoBehaviour {
 
 	private Rigidbody rb;
     public float gravity = -6;
-	public bool isChanging;
+	public bool isChanging = false;
+	//public float rotationSpeed = 1;
+	private AutoMoveAndRotate autoMove;
+	private FirstPersonController FPC;
+	private Camera camera;
 
 	void Start()
 	{
-		
+		autoMove = GetComponent<AutoMoveAndRotate>();
+
+		FPC = GetComponent <FirstPersonController> ();
 		rb = GetComponent<Rigidbody>();
-		isChanging = false;
+		camera = GetComponent<Camera> ();
+		autoMove.enabled = false;
 	}
 
 	void FixedUpdate()
@@ -23,8 +31,13 @@ public class Gravity : MonoBehaviour {
            ChangeGravity();
         }
 
-        rb.AddForce(0, gravity*5, 0);
+     rb.AddForce(0, gravity*5, 0);
 
+		//if (!inverted)
+		//camera.transform.localRotation = Quaternion.identity;
+		//else
+		camera.transform.localRotation = rb.transform.localRotation;
+			
 
 
     }
@@ -35,13 +48,51 @@ void ChangeGravity()
 {
 		isChanging = true;
 		Cursor.lockState = CursorLockMode.Locked;
-
 		gravity *= -1;
+		StartCoroutine (SpinPlayer ());
 
-		transform.localRotation = Quaternion.Euler(180, 0, 0);
+
+		//void FixedUpdate() {
+			//float turn = Input.GetAxis("Horizontal");
+			//rb.AddTorque(transform.right * rotationSpeed * turn);
+		//rb.AddTorque(180, 0, 0, ForceMode.VelocityChange);
+	
+
+		//transform.eulerAngles = new Vector3(0, 0, 180);
+		//void Update() {
+		//for (int i = 0; i < 180; i++)
+			//rb.transform.rotate(Quaternion.Slerp(Quaternion.identity, Quaternion.Euler(180,0,0), Time.fixedDeltaTime * rotationSpeed));
+			//rb.transform.Rotate(Vector3.right, Time.deltaTime, Space.World);
+		//}
+	
+
+
+
+
+
+
+
+		//transform.localRotation = Quaternion.Euler(180, 0, 0);
 
 		Cursor.lockState = CursorLockMode.None;
 		isChanging = false;
+}
+
+
+
+
+
+
+	IEnumerator SpinPlayer(){
+
+
+
+		FPC.enabled = false;
+		autoMove.enabled = true;
+		yield return new WaitForSeconds (2);
+		autoMove.enabled = false;
+		FPC.enabled = true;
+
 }
 
 }
